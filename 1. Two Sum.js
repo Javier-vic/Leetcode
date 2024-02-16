78f5c20e75f6429b62768de9f145dc9174d763fa
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2024-02-16 19:36:02 -0300

    1. Two Sum

---
+++ 1. Two Sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+        // if (nums[i] <= target) {
+            for(let j = i+1; j < nums.length; j++){              
+                if (nums[i] + nums[j] == target) {
+                    return [i, j];
+                }
+            }
+        // }
+    }
+};
+
+let aux = twoSum([-1,-2,-3,-4,-5], -8);
+console.log(aux);
