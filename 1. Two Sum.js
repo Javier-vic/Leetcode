06001551e04546e17cb05896ef5e8761b982c4e2
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2024-02-16 19:52:20 -0300

    Corrección 1.

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -5,13 +5,11 @@
  */
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++){
-        // if (nums[i] <= target) {
-            for(let j = i+1; j < nums.length; j++){              
-                if (nums[i] + nums[j] == target) {
-                    return [i, j];
-                }
+        for(let j = i+1; j < nums.length; j++){              
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
             }
-        // }
+        }
     }
 };
 
